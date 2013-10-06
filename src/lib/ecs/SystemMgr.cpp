26a612f2a0f819811b3147c113f848fca056f23c
Babis <esrever2357@gmail.com>
2013-10-06 14:15:29 +0100

    moef

--- src/lib/ecs/SystemMgr.cpp
+++ src/lib/ecs/SystemMgr.cpp
@@ -1,4 +1,6 @@
 #include "SystemMgr.h"
+#include "ecs.h"
+#include "ËntityMgr.h"
 
 #include "ComponentQuery.h"
 
@@ -7,10 +9,24 @@ namespace pgn
 	void cSystemMgr::from_json(const rapidjson::Value& zRoot)
 	{
 		const auto&  qobj = zRoot["Queries"];
-		for (auto itr = qobj.Begin(); itr != qobj.End(); ++itr) 
+		for (auto itr = qobj.MemberBegin(); itr != qobj.MemberEnd(); ++itr) 
 		{
+			// get a query object
 			const auto& qcur = *itr;
-			//qcur.GetName
+			const auto& qname = qcur.name;
+			const auto& types = qcur.value;
+			component_mask_type mask;
+			for (auto itr2 = types.Begin(); itr2 != types.End(); ++itr2) 
+			{
+				const auto& s = itr2->GetString();
+				auto idx = EMGR.GetComponentTypeIndex(s);
+				if(idx != 0xFFFFFFFF)
+					mask.set(idx);
+				else
+					; // TODO: handle errors
+			}
+			mComponentQueries.insert( cComponentQuery(mask));
+			// TODO: add names to queries! I already grabbed it
 		}
 		const auto&  sobj = zRoot["Systems"];
 	}
