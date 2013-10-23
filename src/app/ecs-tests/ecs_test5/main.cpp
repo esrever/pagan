e8febcf3f11d61282c49b5af710fe09dff4bf78d
Babis <esrever2357@gmail.com>
2013-10-23 17:35:27 +0100

    asd

--- src/app/ecs-tests/ecs_test5/main.cpp
+++ src/app/ecs-tests/ecs_test5/main.cpp
@@ -36,9 +36,12 @@ int main()
 	//# Curses init
 
 	initscr();			/* Start curses mode 		*/
+	start_color();
 	raw();				/* Line buffering disabled	*/
 	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
 	noecho();			/* Don't echo() while we do getch */
+
+	auto cnum = tigetnum("colors");
 
 	//##############################
 	//# Loop
