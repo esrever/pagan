73c4d0ced209d6a7f38b982495990411c322a221
Babis <esrever2357@gmail.com>
2013-10-28 15:38:01 +0000

    asd

--- src/lib/rl/actions/common/MoveAdjacent.cpp
+++ src/lib/rl/actions/common/MoveAdjacent.cpp
@@ -12,15 +12,21 @@ namespace pgn
 {
 	void MoveAdjacent(cEntity e, int x, int y)
 	{
-		// TODO: Change the entity's location, if possible. Emit messages to guide text log, e.g. "{e.name} bumped onto ${e2.name}"
-		auto& compo = ECS.mEntityMgr->GetComponents(e);
-
-		std::shared_ptr<cComponent<cLocation>> locPtr;
-		compo.GetComponent(locPtr);
 		
-		// TODO: Get current level. Should be cheaply available
-		locPtr->mData.mCoords;
+		// TODO: All this calls a Move(e, loc)? prob not, as in cases of Push, I need to have a last position which is adjacent. But then, grab the last free in the path?
+		// TODO: Change the entity's location, if possible. Emit messages to guide text log, e.g. "{e.name} bumped onto ${e2.name}"
 
+		auto& ecompo = ECS.mEntityMgr->GetComponents(e);
+		std::shared_ptr<cComponent<cLocation>> locPtr;
+		ecompo.GetComponent(locPtr);
+		
+		auto& coords = locPtr->mData.mCoords;
+
+		// TODO: Initially, ignore everything. Just set the new coordinates
+		coords.x += x;
+		coords.y += y;
+
+		// TODO: Get current level. Should be cheaply available
 		// TODO: Query coords for tile type
 		// TODO: Query coords for other entities
 		// TODO: If tile type has IsObstacle && its true, we're blocked. 
