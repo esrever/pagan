f91414297b137295508d92eef63685aec6b8abab
Babis <esrever2357@gmail.com>
2013-10-29 08:28:50 +0000

    asd

---
+++ src/lib/rl/actions/QuitProgramAction.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "QuitProgramAction.h"
+#include "../events/events.h"
+
+namespace pgn
+{
+	namespace action
+	{
+		void QuitProgram(cEntity e) 
+		{
+			cExitApplicationEventData::emit(); 
+		}
+	}
+}
