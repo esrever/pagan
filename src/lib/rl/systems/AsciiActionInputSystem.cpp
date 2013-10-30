74b79aa928251f80b983fa9e010e10d67aec5528
Babis <esrever2357@gmail.com>
2013-10-30 21:29:01 +0000

    asd

--- src/lib/rl/systems/AsciiActionInputSystem.cpp
+++ src/lib/rl/systems/AsciiActionInputSystem.cpp
@@ -2,13 +2,43 @@
 
 #include "AsciiActionInputSystem.h"
 
+#include <curses.h>
+
+#include <ecs/ecs.h>
+#include <ecs/SystemMgr.h>
+
+#include <rl/components/KeyActionMapper.h>
+
 namespace pgn
 {
 	//------------------------------------------------------
 	void cAsciiActionInputSystem::Process()
 	{
-		// TODO: simple turn taking at the moment.
-		// Move both entities to a random position
+		auto ch = static_cast<wchar_t>(getch());
+		// Get the query. This should be easily retrievable
+		auto qit = ECS.mSystemMgr->GetQueries().find("InputListener");
+		if( qit != ECS.mSystemMgr->GetQueries().end())
+		{
+			// Get entities found by the query
+			auto ents = qit->second.Get();
+			for(auto e : ents)
+			{
+				// Get the entities' components
+				const auto& ec = ECS.mEntityMgr->GetComponents(e);
+
+				// Get keyactionmapper component
+				std::shared_ptr< cComponent<cKeyActionMapper>> ptr;
+				ec.GetComponent( ptr);
+				
+				// find function
+				auto itFunc = ptr->mData.mActions.find(ch);
+				if(itFunc != ptr->mData.mActions.end() )
+				{
+					// if found, execute
+					itFunc->second(e);
+				}
+			}
+		}
 	}
 
 	//------------------------------------------------------
--- src/lib/rl/systems/MapInputSystem.cpp
+++ src/lib/rl/systems/MapInputSystem.cpp
@@ -18,50 +18,6 @@ namespace pgn
 		// TODO: the listener will check 1) if it's active 2) against a map of keys to:
 		//		 void function(), or a signal
 		chtype ch = getch();
-		// TODO: InputKeyboard component: 
-		glm::ivec2 gotoDir(0,0);
-		switch(ch)
-		{
-			case 27: 
-				cExitApplicationEventData::emit(); 
-				break;
-			case '1':
-			case KEY_END:
-			case KEY_C1:
-				gotoDir.x--;gotoDir.y--; break;
-			case '2': 
-			case KEY_DOWN:
-			case KEY_C2:
-				gotoDir.x  ;gotoDir.y--; break;
-			case '3': 
-			case KEY_NPAGE:
-			case KEY_C3:
-				gotoDir.x++;gotoDir.y--; break;
-			case '7': 
-			case KEY_HOME:
-			case KEY_A1:
-				gotoDir.x--;gotoDir.y++; break;
-			case '8': 
-			case KEY_UP:
-			case KEY_A2:
-				gotoDir.x  ;gotoDir.y++; break;
-			case '9': 
-			case KEY_PPAGE:
-			case KEY_A3:
-				gotoDir.x++;gotoDir.y++; break;
-			case '4':
-			case KEY_LEFT:
-			case KEY_B1:
-				gotoDir.x--;gotoDir.y  ; break;
-			case '5': 
-			case KEY_B2:
-				gotoDir.x  ;gotoDir.y  ; break;
-			case '6': 
-			case KEY_RIGHT:
-			case KEY_B3:
-				gotoDir.x++;gotoDir.y  ; break;
-			default: break;
-		};
 		auto & ecs = ECS;		
 		ecs.mLog.Dbg(boost::str(boost::format("Pressed key '%d'")%ch));
 		if(ch == 27) cExitApplicationEventData::emit();
