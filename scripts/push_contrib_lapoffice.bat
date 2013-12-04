6035bcb32b8bc584f25aee9063ea8eb2d252e90d
Babis <esrever2357@gmail.com>
2013-12-04 16:19:49 +0000

    asd

---
+++ scripts/push_contrib_lapoffice.bat
@@ -0,0 +1,1 @@
+update_contrib.py ..\src\contrib %1 C:\Users\babis\Dropbox\TODOs\roguelike-design\contrib %2
--- scripts/update_contrib.py
+++ scripts/update_contrib.py
@@ -3,18 +3,31 @@ import os
 import time
 import datetime
 import calendar
+import shutil
 
-days_diff = int(sys.argv[2])
+days_diff = float(sys.argv[2])
+start_folder = sys.argv[1]
+out_folder = sys.argv[3]
 
-for root, dirs, files in os.walk( sys.argv[1] ):
+testOnly = len(sys.argv) > 4;
+
+for root, dirs, files in os.walk( start_folder ):
     for basename in files:
         filename = os.path.join(root, basename)
         
         tfile = os.path.getmtime(filename);
         tnow = calendar.timegm(time.gmtime())
-        
         tdiff = abs(tfile - tnow);
-        
         day_diff = tdiff/(60*60*24)
         if(day_diff < days_diff):
-            print filename
+            (fdir, fname) = os.path.split(filename)
+            fdir_rel = os.path.relpath(fdir,start_folder)
+            fdir_out = os.path.join(out_folder, fdir_rel);
+            fname_out = os.path.join(fdir_out, fname);
+            if not os.path.exists(fdir_out):
+                os.makedirs(fdir_out)
+            if not testOnly:
+                shutil.copyfile(filename, fname_out)
+            else:
+                print fname_out
+            
