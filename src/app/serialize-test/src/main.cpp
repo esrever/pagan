fbf6523e093d06a79dc3e21d3675edca45a71765
Babis <esrever2357@gmail.com>
2014-02-28 08:48:57 +0000

    asd

--- src/app/serialize-test/src/main.cpp
+++ src/app/serialize-test/src/main.cpp
@@ -30,4 +30,11 @@ int main(int argc, char ** argv)
 	pugi::xml_document doc_in;
 	doc_in.load_file("testmich.xml");
 	doc_in.print(std::cout);
+
+	pugi::xml_document doc_in2;
+	auto res = doc_in2.load_file("C:\\Users\\Babis\\Documents\\GitHub\\pagan\\src\\lib\\ecs\\data\\example.xml");
+	if (!res)
+		std::cout << res.description() << std::endl;
+	else
+		doc_in2.print(std::cout);
 }
