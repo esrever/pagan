7c26ce39dc03dc950f49daf766ff62447ee8f9e6
Babis <esrever2357@gmail.com>
2014-03-17 21:10:11 +0000

    ad

--- src/lib/rlut/components/Visibility.h
+++ src/lib/rlut/components/Visibility.h
@@ -13,9 +13,11 @@ namespace pgn{
             struct cVisibility
             {
                 cVisibility(const std::map< int, cArray2D<bool> >& arg0 = std::map< int, cArray2D<bool> >(),
-                    const std::set<glm::ivec2>& arg1 = std::set<glm::ivec2>()):
+                    const std::map< int, cArray2D<bool> >& arg1 = std::map< int, cArray2D<bool> >(),
+                    const std::set<glm::ivec2>& arg2 = std::set<glm::ivec2>()):
                         mExplored(arg0),
-                        mVisible(arg1){}
+                        mVisible(arg1),
+                        mVisibleSet(arg2){}
                 
                 size_t SerializeIn(const node_type&);
                 void SerializeOut(node_type&) const;
@@ -23,8 +25,11 @@ namespace pgn{
                 //!Explored flag for levels
                 std::map< int, cArray2D<bool> > mExplored;
                 
+                //!Visible flag for levels
+                std::map< int, cArray2D<bool> > mVisible;
+                
                 //!Visible tiles
-                std::set<glm::ivec2> mVisible;
+                std::set<glm::ivec2> mVisibleSet;
                 
             };
         
