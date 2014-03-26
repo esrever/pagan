d93f7cbb831b1cc4b0870d89611eedb3b2a101f8
Babis <esrever2357@gmail.com>
2014-03-26 09:44:25 +0000

    asd

--- src/lib/rl/systems/StatsProc.cpp
+++ src/lib/rl/systems/StatsProc.cpp
@@ -9,6 +9,21 @@ namespace pgn
 	{
 		namespace sys
 		{
+			/*
+				evade = 10+ mod(dex)
+				att = 1d20 + mod(dex)
+				dmg = 1d8 + mod(str)
+				hit = attacker.att >= defender.evade
+				phys_crit_chance = 5% + mod(dex)%
+				phys_crit_dmg = 100% + 10*(mod(str) + mod(dex))%
+
+				gain level:
+					random +1 of (str,dex,con)
+					add hit die
+					recalc maxhealth
+					refill current health
+			*/
+
 			//-------------------------------------------------------------
 			bool cStatsProc::operator()()
 			{
@@ -18,6 +33,7 @@ namespace pgn
 			//-------------------------------------------------------------
 			int cStatsProc::CalcNewLevelHP(ecs::cEntityWithData)
 			{
+				// roll HD and add StatModifier( stats[con] )
 				return 1;
 			}
 
@@ -30,6 +46,7 @@ namespace pgn
 			//-------------------------------------------------------------
 			int cStatsProc::PhysDmg(ecs::cEntityWithData ed)
 			{
+				// roll 1d8 and add StatModifier( stats[str])
 				auto stats = ed->second.Component<cmp::cStats>();
 				return 0;
 			}
@@ -45,12 +62,16 @@ namespace pgn
 			//-------------------------------------------------------------
 			float cStatsProc::CritChance(ecs::cEntityWithData)
 			{
+				// roll 1d20 and add StatModifier( stats[dex])
 				return 0.0f;
 			}
 
 			//-------------------------------------------------------------
 			float cStatsProc::HitChance(ecs::cEntityWithData)
 			{
+				// everyone starts with 10 evade value
+				// 
+				// roll 1d20 and add StatModifier( stats[dex])
 				return 0.0f;
 			}
 
